686e98bef8350f5ea0fd981c269cff9bc7d76097
mario <marioroos2502@gmail.com>
2023-11-08 19:15:57 +0100

    añadida previsualización de los elementos seleccionados para el merge

--- src/static/merge.js
+++ src/static/merge.js
@@ -1,27 +1,40 @@
-document.getElementById('pdfForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    var form = this;
-    var formData = new FormData(form);
+document.addEventListener('DOMContentLoaded', () => {
+    const pdfForm = document.getElementById('pdfForm');
+    const preview = document.getElementById('preview');
 
-    fetch(form.action, {
-        method: form.method,
-        body: formData
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
+    pdfForm.addEventListener('change', async (event) => {
+        const files = event.target.files;
+        for (const file of files) {
+            const reader = new FileReader();
+            reader.onload = async (e) => {
+                const arrayBuffer = e.target.result;
+                const pdf = await pdfjsLib.getDocument(arrayBuffer).promise;
+                const page = await pdf.getPage(1); // Obtiene la primera página
+
+                const viewport = page.getViewport({ scale: 0.2 }); // Ajusta la escala según sea necesario
+                const canvas = document.createElement('canvas');
+                const context = canvas.getContext('2d');
+                canvas.width = viewport.width;
+                canvas.height = viewport.height;
+
+                const renderContext = {
+                    canvasContext: context,
+                    viewport: viewport
+                };
+                await page.render(renderContext).promise;
+
+                const listItem = document.createElement('div');
+                listItem.classList.add('preview-item');
+
+                const fileName = document.createElement('div');
+                fileName.classList.add('file-name');
+                fileName.textContent = file.name;
+
+                listItem.appendChild(canvas);
+                listItem.appendChild(fileName);
+                preview.appendChild(listItem);
+            };
+            reader.readAsArrayBuffer(file);
         }
-        return response.blob();
-    })
-    .then(blob => {
-        var url = window.URL.createObjectURL(blob);
-        var a = document.createElement('a');
-        a.href = url;
-        a.download = 'output.pdf';
-        document.body.appendChild(a);
-        a.click();
-        a.remove();
-        alert("El PDF se ha generado con éxito.");
-    })
-    .catch(error => console.error('Error:', error));
+    });
 });
